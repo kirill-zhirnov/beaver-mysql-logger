5d0ae51fcfdce7b56fd1dc1b8e3eb5a86f95daf1
= <=>
2014-07-01 11:05:28 +0400

    assets completed

--- public/protected/project/views/layout.php
+++ public/protected/project/views/layout.php
@@ -4,23 +4,8 @@
 		<meta charset="utf-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
-
-<!--		<link rel="stylesheet" href="/public/assets/libs/twitterBootstrap/css/bootstrap.min.css">-->
-		<link rel="stylesheet" href="/public/compiled-assets/css/styles.css" />
-
-		<?php //@todo: fixme!?>
-		<script type="text/javascript" src="/public/assets/libs/jquery-2.1.1.min.js"></script>
-		<script type="text/javascript" src="/public/assets/libs/extend.js"></script>
-		<script type="text/javascript" src="/public/assets/libs/kz/util.js"></script>
-		<script type="text/javascript" src="/public/assets/libs/kz/app.js"></script>
-		<script type="text/javascript" src="/public/assets/libs/kz/widget.js"></script>
-		<script type="text/javascript" src="/public/assets/libs/kz/form.js"></script>
-		<script type="text/javascript" src="/public/assets/libs/kz/loading.js"></script>
-		<script type="text/javascript" src="/public/assets/libs/kz/ajaxResponse.js"></script>
-		<script type="text/javascript" src="/public/assets/libs/kz/modal.js"></script>
-		<script type="text/javascript" src="/public/assets/libs/kz/modalMsg.js"></script>
-		<script type="text/javascript" src="/public/assets/libs/jquery/jquery.form.js"></script>
-		<script src="/public/assets/libs/twitterBootstrap/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" href="/public/assets/compiled/css/styles.css" />
+		<script type="text/javascript" src="/public/assets/compiled/js/scripts.js"></script>
 		<script type="text/javascript">
 			var app = null;
 			$(function() {
